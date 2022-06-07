ce671b6817c8da64d445894efb29b925f0e370bc
Eduardo <edusoares1992@gmail.com>
2022-06-06 23:23:34 -0300

    Rotas 1.1

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminProductController;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/product',[ProductController::class, 'show']);
+
+Route::get('/admin/product', [AdminProductController::class, 'index']);
+Route::get('/admin/product/edit', [AdminProductController::class, 'edit']);
+
